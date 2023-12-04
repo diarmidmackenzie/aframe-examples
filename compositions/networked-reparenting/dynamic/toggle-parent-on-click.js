7555641fd1fa6c44f229ea2991330965e6d4a41b
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-04 12:25:46 +0000

    Rework to allow either client to parent/unparent

--- compositions/networked-reparenting/dynamic/toggle-parent-on-click.js
+++ compositions/networked-reparenting/dynamic/toggle-parent-on-click.js
@@ -1,26 +1,27 @@
 // Also requires:
 // <script src="https://cdn.jsdelivr.net/npm/aframe-connecting-line@0.1.0/index.min.js"></script>
 
-AFRAME.registerComponent('toggle-reparent-on-space', {
+AFRAME.registerComponent('toggle-parent-on-click', {
 
   schema: {
+    button: { type: 'selector'},
     parent: { type: 'selector'},
     debug: {default: false}
   },
 
   init() {
-    window.addEventListener('keydown', (e) => {
-      if (e.key === " ") {
-        this.toggleParent()
-      }
-    })
+    this.data.button.onclick = this.toggleParent.bind(this)
+    
   },
 
   toggleParent() {
 
+    NAF.utils.takeOwnership(this.el)
+
     if (this.el.hasAttribute('object-parent')) {
       this.el.removeAttribute('object-parent')
       this.el.removeAttribute('connecting-line');
+      this.data.button.innerHTML = "Parent"
     }
     else {
       this.el.setAttribute('object-parent', `parentId: ${this.data.parent.id}`)
@@ -30,5 +31,16 @@ AFRAME.registerComponent('toggle-reparent-on-space', {
                                                  end: #${this.data.parent.id}`)
       }
     }
+  },
+
+  tick() {
+
+    if (this.el.hasAttribute('object-parent')) {
+      this.data.button.innerHTML = "Unparent"
+    }
+    else {
+      this.data.button.innerHTML = "Parent"
+    }
+
   }
 })
